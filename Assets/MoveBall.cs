0ca44f5bb2019541ab8993926c7258f68bf7f1d4
dat17-05-ishiduki <175b005gp12@gmail.com>
2017-06-09 15:36:36 +0900

    物理入れた、、

---
+++ Assets/MoveBall.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveBall : MonoBehaviour {
+	public float vx = -10f;
+	public float vy = -10f;
+	private Rigidbody rig;
+
+	// Use this for initialization
+	void Start () {
+		rig = GetComponent<Rigidbody> ();
+		rig.velocity = new Vector3 (vx, vy, 0f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
