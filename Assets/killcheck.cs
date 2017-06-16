353f5cffbde7d0d078cb13fc0cd5e564ee4a9940
dat17-05-ishiduki <175b005gp12@gmail.com>
2017-06-16 16:15:29 +0900

    ballを消したり

---
+++ Assets/killcheck.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class killcheck : MonoBehaviour {
+
+	/*// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}*/
+
+	//使わない場合はstart updateは消してもよい↑
+	//マウスが重なった時の処理
+	void OnMouseEnter(){
+		//ぶつかったら消す
+		//destroy(=実体)→そのものを消す
+
+		Debug.Log("hit");
+		Destroy (gameObject);
+		//gameObject.SetActive(false);動作無効。非表示
+		//生成。削除は処理が重い、、、
+}
+}
---
+++ Assets/manager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class manager : MonoBehaviour {
+
+	public Text scoreText;
+	int iTime = 0;
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		iTime++;
+		scoreText.text = "" + iTime;
+	}
+}
